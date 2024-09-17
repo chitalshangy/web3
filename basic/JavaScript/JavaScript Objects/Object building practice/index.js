bc07bbecd88e05d90d332a2d2adc14be41559a9b
chitalshangy <1129385201@qq.com>
2024-09-17 21:09:16 +0800

    feat : add features to bouncing balls demo

--- basic/JavaScript/JavaScript Objects/Object building practice/index.js
+++ basic/JavaScript/JavaScript Objects/Object building practice/index.js
@@ -6,6 +6,13 @@ const height = (canvas.height = window.innerHeight);
 
 const balls = [];
 
+const p = document.querySelector("p");
+let ballNum = 0;
+
+const EVIL_VEL = 20;
+const EVIL_SIZE = 10;
+const EVIL_LINE_WIDTH = 3;
+
 function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -14,12 +21,21 @@ function randomRGB() {
     return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball {
-    constructor(x, y, velX, velY, color, size) {
+class Shape {
+    constructor(x, y, velX, velY) {
         this.x = x;
         this.y = y;
         this.velX = velX;
         this.velY = velY;
+    }
+}
+
+class Ball extends Shape {
+
+    exists = true;
+
+    constructor(x, y, velX, velY, color, size) {
+        super(x, y, velX, velY);
         this.color = color;
         this.size = size;
     }
@@ -54,7 +70,7 @@ class Ball {
 
     collisionDetect() {
         for (let ball of balls) {
-            if (this !== ball) {
+            if (this !== ball && ball.exists) {
                 let dx = this.x - ball.x;
                 let dy = this.y - ball.y;
                 let distance = Math.sqrt(dx * dx + dy * dy);
@@ -64,10 +80,88 @@ class Ball {
             }
         }
     }
+}
 
-    toString() {
-        console.log(`x: ${this.x}, y: ${this.y}, velX: ${this.velX}, velY: ${this.velY}, color: ${this.color}, size: ${this.size}.`);
+class EvilCircle extends Shape {
+
+    constructor(x, y) {
+        super(x, y, EVIL_VEL, EVIL_VEL);
+        this.color = "rgb(255, 255, 255)";
+        this.size = EVIL_SIZE;
+
+        window.addEventListener("keydown", (e) => {
+            switch (e.key) {
+                case "a": this.x -= this.velX; break;
+                case "d": this.x += this.velX; break;
+                case "w": this.y -= this.velY; break;
+                case "s": this.y += this.velY; break;
+            }
+        })
     }
+
+
+    draw() {
+        ctx.beginPath();
+        ctx.lineWidth = EVIL_LINE_WIDTH;
+        ctx.strokeStyle = this.color;
+        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
+
+    checkBounds() {
+        if ((this.x + this.size) >= width) {
+            this.x -= this.size;
+        }
+
+        if ((this.x - this.size) <= 0) {
+            this.x += this.size;
+        }
+
+        if ((this.y + this.size) >= height) {
+            this.y -= this.size;
+        }
+
+        if ((this.y - this.size) <= 0) {
+            this.y += this.size;
+        }
+    }
+
+    collisionDetect() {
+        for (let ball of balls) {
+            if (ball.exists) {
+                let dx = this.x - ball.x;
+                let dy = this.y - ball.y;
+                let distance = Math.sqrt(dx * dx + dy * dy);
+                if (distance < this.size + ball.size) {
+                    ball.exists = false;
+                    displayBallNum(ballNum--);
+                }
+            }
+        }
+    }
+}
+
+function loop() {
+    ctx.fillStyle = "rgb(0 0 0 / 0.25)";
+    ctx.fillRect(0, 0, width, height);
+
+    evilCircle.draw();
+    evilCircle.checkBounds();
+    evilCircle.collisionDetect();
+
+    for (let ball of balls) {
+        if (ball.exists) {
+            ball.draw();
+            ball.update();
+            ball.collisionDetect()
+        }
+    }
+
+    requestAnimationFrame(loop);
+}
+
+function displayBallNum(ballNum) {
+    p.textContent = `Ball count: ${ballNum}`;
 }
 
 while (balls.length < 25) {
@@ -81,19 +175,9 @@ while (balls.length < 25) {
         size,
     );
     balls.push(ball);
+    displayBallNum(ballNum++);
 }
 
-function loop() {
-    ctx.fillStyle = "rgb(0 0 0 / 0.25)";
-    ctx.fillRect(0, 0, width, height);
-
-    for (let ball of balls) {
-        ball.draw();
-        ball.update();
-        ball.collisionDetect()
-    }
-
-    requestAnimationFrame(loop);
-}
+let evilCircle = new EvilCircle(random(0, width), random(0, height));
 
 loop();
